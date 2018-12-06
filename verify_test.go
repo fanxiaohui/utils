f33a42ea0323e2931597f24989dca934ebca2e3e
mo <jgb40@qq.com>
2018-12-06 20:36:08 +0800

    采用g名库oconvey测试,更加直观 测试crc 和异或

--- verify_test.go
+++ verify_test.go
@@ -1,12 +1,25 @@
 package common
 
-import "testing"
+import (
+	"testing"
 
-func TestCrc(t *testing.T) {
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestCrcChecksum(t *testing.T) {
 	var tab = []byte{0x04, 0x00, 0x00, 0x00, 0x03, 0xb0, 0x0b}
-	if CrcChecksum(tab) != 0xc79a {
-		t.Log("failed")
-	} else {
-		t.Log("success")
-	}
+
+	// Only pass t into top-level Convey calls
+	Convey("crc校验", t, func() {
+		So(0xc79a, ShouldEqual, CrcChecksum(tab))
+	})
+}
+
+func TestXorCheckSum(t *testing.T) {
+	var tab = []byte{0x04, 0x00, 0x00, 0x00, 0x03, 0xb0, 0x0b}
+
+	// Only pass t into top-level Convey calls
+	Convey("crc校验", t, func() {
+		So(0xbc, ShouldEqual, XorCheckSum(tab))
+	})
 }
