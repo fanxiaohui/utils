8348bbce1488e0ebfde4018e95c232cdee2ca8ba
mo <jgb40@qq.com>
2018-12-09 22:27:32 +0800

    提供运行时间 提供fix版本号

--- buildtime.c
+++ buildtime.c
@@ -2,14 +2,16 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
+
+static const char *months[] = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+static char buildtime[20];
+static char fixversion[9];
+
 /*
 * __DATE__ : May 12 2017  月日年
 * __TIME__ : 15:26:26     时分秒
 * return : 2018-12-09 15:26:26
 */
-static const char *months[] = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
-static char buildtime[20];
-
 const char *Buildtime(void)
 {
 	uint8_t i,lengh;
@@ -31,4 +33,27 @@ const char *Buildtime(void)
 	(void)sprintf(buildtime,"%s-%02d-%02d %s", year, month, day, __TIME__);
 	
     return buildtime;
+}
+
+// fix version
+const char *FixVersion(void)
+{
+	uint8_t i,lengh;
+	char *pstr = __DATE__;
+	char *year = &pstr[strlen(pstr) - 4];
+	uint8_t month;
+	uint8_t day;
+	
+	for(i = 0;i < sizeof(months)/sizeof(months[0]); i++){
+        if(memcmp(months[i],pstr,3) == 0){
+            month = i + 1;
+            break;
+        }
+    }
+	
+	day = pstr[4] == ' ' ? 0 : (pstr[4] - 0x30);
+	day = day * 10 + pstr[5] - 0x30;
+	(void)sprintf(fixversion,"%s%02d%02d", year, month, day);
+		
+	return fixversion;
 }
--- common.go
+++ common.go
@@ -1,9 +1,42 @@
 package common
 
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
 //const char *Buildtime(void);
+//const char *FixVersion(void);
 import "C"
+
+var markStartTime time.Time = time.Now()
+
+// Largest time is 2540400:10:10
+func RunningTime() string {
+	// get second
+	u := uint64(time.Since(markStartTime) / time.Second)
+
+	second := u % 60
+	u /= 60
+	minutes := u % 60
+	u /= 60
+
+	return fmt.Sprintf("%02d:%02d:%02d", u, minutes, second)
+}
 
 /*format : 2018-12-09 15:26:26*/
 func BuildTime() string {
 	return C.GoString(C.Buildtime())
+}
+
+/*format : major.minor.buildDate - 1.0.20181209 */
+func Version(major, minor string) string {
+	version := []string{
+		major,
+		minor,
+		C.GoString(C.FixVersion()),
+	}
+
+	return strings.Join(version, ".")
 }
