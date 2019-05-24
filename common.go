7bbeea48aa14f60641ea9af3c8ca36dd8e69d1ac
mo <thinkgo@aliyun.com>
2019-05-24 21:24:03 +0800

    去除多余api

--- common.go
+++ common.go
@@ -1,41 +1,12 @@
 package utils
 
 import (
-	"fmt"
-	"time"
 	"unsafe"
 )
 
 //const char *Buildtime(void);
 //const char *VersionDate(void);
 import "C"
-
-var markStartTime = time.Now().Local()
-
-const layout = "2006-01-02 15:04:05"
-
-// SetupTime 开机时间
-func SetupTime() string {
-	return markStartTime.Format(layout)
-}
-
-// RunningTimeSeconds 运行时间 秒数
-func RunningTimeSeconds() uint64 {
-	return uint64(time.Since(markStartTime) / time.Second)
-}
-
-// RunningTime 运行时间 Largest time is 2540400:10:10
-func RunningTime() string {
-	// get second
-	u := uint64(time.Since(markStartTime) / time.Second)
-
-	second := u % 60
-	u /= 60
-	minutes := u % 60
-	u /= 60
-
-	return fmt.Sprintf("%02d:%02d:%02d", u, minutes, second)
-}
 
 // BuildDateTime 编译时间 format : 2018-12-09 15:26:26
 func BuildDateTime() string {
--- common_test.go
+++ common_test.go
@@ -7,22 +7,6 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestSetupTime(t *testing.T) {
-	Convey("开机时间格式: 2006-01-02 15:04:05", t, func() {
-		println(SetupTime())
-	})
-}
-
-func TestRunningTime(t *testing.T) {
-	Convey("运时时间格式和最大值: 2540400:10:10", t, func() {
-		println(RunningTime())
-	})
-	Convey("运时j时秒数", t, func() {
-		println(RunningTimeSeconds())
-	})
-
-}
-
 func TestBuildTime(t *testing.T) {
 	Convey("编译时间格式: 2018-12-09 15:26:26", t, func() {
 		println(BuildDateTime())
