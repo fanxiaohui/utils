c579a906230ae76b0816c354c55a7f742ca0fe1e
mo <thinkgo@aliyun.com>
2019-05-29 16:12:49 +0800

    去除c文件 解决方案采用 go build -ldflags "-X main.BuildTime=`date +%F.%T.%z`" 生成编译时间

--- common.go
+++ common.go
@@ -4,15 +4,6 @@ import (
 	"unsafe"
 )
 
-//const char *Buildtime(void);
-//const char *VersionDate(void);
-import "C"
-
-// BuildDateTime 编译时间 format : 2018-12-09 15:26:26
-func BuildDateTime() string {
-	return C.GoString(C.Buildtime())
-}
-
 // IsMachineLittleEndian 判断系统大小端
 func IsMachineLittleEndian() bool {
 	var i int16 = 0x0001
