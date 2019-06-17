22fc426676211f07459e67abcee4b156a7fab787
mo <thinkgo@aliyun.com>
2019-06-17 09:44:41 +0800

    改成bash

--- cover.sh
+++ cover.sh
@@ -2,4 +2,4 @@ mkdir -p coverHtml
 go test -coverpkg=./... -coverprofile=coverHtml/coverage.data ./...
 go tool cover -html=coverHtml/coverage.data -o coverHtml/coverage.html
 go tool cover -func=coverHtml/coverage.data -o coverHtml/coverage.txt
-/bin/sh -c 'firefox $PWD/coverHtml/coverage.html'
+/bin/bash -c 'firefox $PWD/coverHtml/coverage.html'
